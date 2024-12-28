7538ab90accc411928a49ff9bed66450b2178a63
baohaoliao <liaobaohao@gmail.com>
2024-12-28 15:02:17 +0100

    sd

--- sd.py
+++ sd.py
@@ -125,8 +125,8 @@ def setup(args):
     from transformers import AutoModelForCausalLM, AutoTokenizer
     llm1_tokenizer = AutoTokenizer.from_pretrained(args.draft_model_name_or_path)
     llm2_tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-    llm1 = AutoModelForCausalLM.from_pretrained(args.draft_model_name_or_path, device_map="auto")
-    llm2 = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, device_map="auto")
+    llm1 = AutoModelForCausalLM.from_pretrained(args.draft_model_name_or_path, device_map="auto", attn_implementation="flash_attention_2")
+    llm2 = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, device_map="auto", attn_implementation="flash_attention_2")
 
     llms = [llm1, llm2]
     tokenizers = [llm1_tokenizer, llm2_tokenizer]
