5418806bd8779d8a02be71439e02c80bffa62798
baohaoliao <liaobaohao@gmail.com>
2025-01-21 13:00:27 +0100

    math_eval

--- math_eval.py
+++ math_eval.py
@@ -303,6 +303,7 @@ def main(llm, tokenizer, data_name, args):
                 stop_id_sequences=stop_words,
             )
 
+        print(len(outputs), len(current_prompts))
         assert len(outputs) == len(current_prompts)
 
         # process all outputs
